25befc80723cb5bcb94b048b3bef3ae09c6c59f2
XLEprime <1020134037@qq.com>
2022-05-06 01:00:05 +0800

    完成server中的基础函数

---
+++ include/server.h
@@ -0,0 +1,107 @@
+﻿/**
+ * @file server.h
+ * @author Haolin Yang
+ * @brief 服务器类的声明
+ * @version 0.1
+ * @date 2022-05-05
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <QObject>
+#include <QUdpSocket>
+#include <QJsonValue>
+
+#include "user.h"
+
+//服务器类
+class Server : public QObject
+{
+    Q_OBJECT
+public:
+    Server(QObject *parent, quint16 _port, UserManage *_usermanage);
+
+private:
+    enum RequestType
+    {
+        time,             //系统时间
+        addTime,          //加快系统时间
+        userRegister,     //注册
+        login,            //登录
+        logout,           //登出
+        changePassword,   //修改密码
+        info,             //查看个人信息
+        allUserInfo,      //查看所有用户信息
+        addExpressman,    //添加快递员
+        deleteExpressman, //删除快递员
+        assign,           //为一个快递指定快递员
+        delivery,         //运送一个快递
+        addBalance,       //充值
+        queryallitem,     //查询所有快递
+        query,            //查询所有符合条件的快递
+        queryExpress,     //查询当前快递员所属所有符合条件的快递
+        querysrc,         //查询当前用户发出的的符合条件的快递
+        send,             //发送快递
+        receive           //接收快递
+    };
+
+    /**
+     * @brief 接收message
+     */
+    void messageHandler();
+
+    /**
+     * @brief 将凭据打包成JWT token字符串
+     * @param payload 凭据
+     * @param secret 密钥
+     * @return QString JWT token字符串
+     */
+    QString jwtEncoding(const QJsonObject &payload, const QByteArray &secret) const;
+
+    /**
+     * @brief 验证JWT token的指纹
+     * @param jwt JWT token
+     * @param secret 密钥
+     * @return bool 如果验证成功，返回true
+     */
+    bool jwtVerify(const QString &jwt, const QByteArray &secret) const;
+
+    /**
+     * @brief 从JWT token字符串中提取凭据
+     * @param jwt JWT token
+     * @return 凭据
+     */
+    QJsonObject jwtGetPayload(const QString &jwt) const;
+
+    /**
+     * @brief 构造仅包含字段错误信息的回复
+     * @param ret 回复
+     */
+    void constructRet(QJsonObject &ret) const;
+
+    /**
+     * @brief 构造回复
+     * @param ret 回复
+     * @param response 用户管理类的处理结果
+     * @note 如果response为空，则回复状态正常，否则回复状态异常
+     */
+    void constructRet(QJsonObject &ret, const QString &response) const;
+
+        /**
+     * @brief 构造回复
+     * @param ret 回复
+     * @param response 用户管理类的处理结果
+     * @param result 用户管理类的回复信息
+     * @note 如果response为空，则回复状态正常，并且把result附加到回复中，否则回复状态异常
+     */
+    void constructRet(QJsonObject &ret, const QString &response, const QJsonValue &result) const;
+
+    UserManage *userManage;
+    QUdpSocket socket;
+    const QByteArray secret = "JWTTokenSecret"; // JWT token 加密密钥
+};
+
+#endif
---
+++ src/server.cpp
@@ -0,0 +1,163 @@
+﻿/**
+ * @file server.cpp
+ * @author Haolin Yang
+ * @brief 服务器类的实现
+ * @version 0.1
+ * @date 2022-05-05
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#include <QNetworkDatagram>
+#include <QJsonDocument>
+
+#include "../include/server.h"
+
+Server::Server(QObject *parent, quint16 port, UserManage *_usermanage) : QObject(parent), userManage(_usermanage), socket(this)
+{
+    socket.bind(QHostAddress::LocalHost, port);
+    QObject::connect(&socket, &QUdpSocket::readyRead, this, &Server::messageHandler);
+}
+
+void Server::messageHandler()
+{
+    while (socket.hasPendingDatagrams())
+    {
+        QNetworkDatagram datagram = socket.receiveDatagram(); //数据报
+        QJsonObject json = QJsonDocument::fromJson(datagram.data()).object();
+        QByteArray response;
+        if (!json.contains("type") || !json.contains("payload"))
+            continue;
+        int type = json["type"].toInt();
+
+        switch (type)
+        {
+        case time:
+            /* code */
+            break;
+
+        case addTime:
+            /* code */
+            break;
+        case userRegister:
+            /* code */
+            break;
+        case login:
+            /* code */
+            break;
+        case logout:
+            /* code */
+            break;
+        case changePassword:
+            /* code */
+            break;
+        case info:
+            /* code */
+            break;
+        case allUserInfo:
+            /* code */
+            break;
+        case addExpressman:
+            /* code */
+            break;
+        case deleteExpressman:
+            /* code */
+            break;
+        case assign:
+            /* code */
+            break;
+        case delivery:
+            /* code */
+            break;
+        case addBalance:
+            /* code */
+            break;
+        case queryallitem:
+            /* code */
+            break;
+        case query:
+            /* code */
+            break;
+        case queryExpress:
+            /* code */
+            break;
+        case querysrc:
+            /* code */
+            break;
+        case send:
+            /* code */
+            break;
+        case receive:
+            /* code */
+            break;
+        default:
+            break;
+        }
+
+        qint64 status = socket.writeDatagram(response, datagram.senderAddress(), datagram.senderPort());
+        if (status == -1)
+            qCritical() << "UDP socket出错";
+    }
+}
+
+QString Server::jwtEncoding(const QJsonObject &payload, const QByteArray &secret) const
+{
+    QJsonObject header;
+    header.insert("alg", "HS256"); //签名算法 HS256
+    header.insert("typ", "jwt");   //类型 jwt
+
+    QByteArray header_encoded = QByteArray(QJsonDocument(header).toJson(QJsonDocument::Compact)).toBase64();
+    QByteArray payload_encoded = QByteArray(QJsonDocument(payload).toJson(QJsonDocument::Compact)).toBase64();
+    QByteArray sig = QMessageAuthenticationCode::hash(header_encoded + "." + payload_encoded, secret, QCryptographicHash::Sha256).toBase64();
+
+    return QString(header_encoded + '.' + payload_encoded + '.' + sig);
+}
+
+bool Server::jwtVerify(const QString &jwt, const QByteArray &secret) const
+{
+    auto splited = jwt.split('.');
+    auto header_encoded = splited[0].toUtf8();
+    auto payload_encoded = splited[1].toUtf8();
+    auto sig = splited[2].toUtf8();
+    return sig == QMessageAuthenticationCode::hash(header_encoded + "." + payload_encoded, secret, QCryptographicHash::Sha256).toBase64();
+}
+
+QJsonObject Server::jwtGetPayload(const QString &jwt) const
+{
+    auto splited = jwt.split('.');
+    auto payload = QByteArray::fromBase64(splited[1].toUtf8());
+    return QJsonDocument::fromJson(payload).object();
+}
+
+void Server::constructRet(QJsonObject &ret) const
+{
+    ret.insert("status", false);
+    ret.insert("payload", "字段有误");
+}
+
+void Server::constructRet(QJsonObject &ret, const QString &response) const
+{
+    if (response.isEmpty())
+    {
+        ret.insert("status", true);
+    }
+    else
+    {
+        ret.insert("status", false);
+        ret.insert("payload", response);
+    }
+}
+
+void Server::constructRet(QJsonObject &ret, const QString &response, const QJsonValue &result) const
+{
+    if (response.isEmpty())
+    {
+        ret.insert("status", true);
+        ret.insert("payload", result);
+    }
+    else
+    {
+        ret.insert("status", false);
+        ret.insert("payload", response);
+    }
+}
